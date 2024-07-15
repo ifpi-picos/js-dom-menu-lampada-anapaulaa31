8b045fb64b874cbd96743bcd77e6998f5b254b30
Anapaulaa31 <paulaana7473@gmail.com>
2024-07-15 11:11:25 -0300

    ana paula

--- js/script.js
+++ js/script.js
@@ -5,10 +5,19 @@ const imagem1 = document.getElementById("lampada-01");
 imagem1.addEventListener("click", function () {
   console.log(imagem1.src);
   if (imagem1.src.endsWith("lampada.jpg")) {
-    imagem1.src = "img/lampada-on.jpg";
+    imagem1.src = "lampada-on.jpg";
   } else {
-    imagem1.src = "img/lampada.jpg";
+    imagem1.src = "lampada.jpg";
   }
 });
 
 // Imagem 2
+const imagem2 = document.getElementById("lampada-02");
+imagem2.addEventListener("mouseover", function () {
+ imagem2.src = "img/lampada-on.jpg";
+});
+
+ imagem2.addEventListener("mouseout", function () {
+    imagem2.src = "img/lampada.jpg";
+ });
+
